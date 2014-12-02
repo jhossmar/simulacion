d1bc2bbd8dc8dfb3cb88215e8ad06a435db69aa7
beimarhuarachi <beimar020@gmail.com>
2014-12-02 11:08:23 -0400

    start to implement the simulation of one patient

--- src/com/umss/fcyt/modelo/simulacion/Reloj.java
+++ src/com/umss/fcyt/modelo/simulacion/Reloj.java
@@ -1,5 +1,23 @@
 package com.umss.fcyt.modelo.simulacion;
 
 public class Reloj {
+	int cantidadMinutosDia;
+	
+	public Reloj() {
+		this.cantidadMinutosDia = 1440;
+	}
 
+	public int getCantidadMinutosDia() {
+		return cantidadMinutosDia;
+	}
+
+	public void setCantidadMinutosDia(int cantidadMinutosDia) {
+		this.cantidadMinutosDia = cantidadMinutosDia;
+	}
+	
+	public int horaAleatoria(int limite) {
+		int resultado = (int)(Math.random()*limite);
+		return resultado;
+	}
+	
 }
--- src/com/umss/fcyt/modelo/simulacion/Simulador.java
+++ src/com/umss/fcyt/modelo/simulacion/Simulador.java
@@ -1,22 +1,30 @@
 package com.umss.fcyt.modelo.simulacion;
 
+import com.umss.fcyt.modelo.Cubiculo;
+import com.umss.fcyt.modelo.Paciente;
 import com.umss.fcyt.modelo.SalaDeEspera;
 import com.umss.fcyt.modelo.SalaEmergencias;
+import com.umss.fcyt.modelo.TipoPaciente;
 
 public class Simulador implements Runnable {
 	private SalaEmergencias salaDeEmergencias;
 	
 	private Reloj reloj;
 	
-	private int velocidadMaximaDeSimulacion;
-	private int velocidadMinimaDeSimulacion;
+	private static final int VELOCIDAD_MAXIMA_SIMULACION = 100;
+	private static final int VELOCIDAD_MINIMA_SIMULACION = 1000;
 	
 	private int velocidadActualDeSimulacion;
+	
+	private int tiempoTranscurrido;
 
 	private Thread procesoSimulacion;
 	
 	public Simulador() {
 		this.salaDeEmergencias = salaDeEmergencias;
+		this.reloj = new Reloj();
+		this.velocidadActualDeSimulacion = VELOCIDAD_MINIMA_SIMULACION;
+		this.tiempoTranscurrido = 0;
 	}
 	
 	public Simulador(SalaEmergencias salaEmergencias) {
@@ -26,6 +34,28 @@ public class Simulador implements Runnable {
 	public void iniciarSimulacion() {
 		this.procesoSimulacion = new Thread(this);
 		this.procesoSimulacion.start();
+	}
+	
+
+	@Override
+	public void run() {
+		int contadorMinutos = 0;
+		
+		while(contadorMinutos < reloj.getCantidadMinutosDia()) {
+			try {
+				Thread.sleep(velocidadActualDeSimulacion);
+				System.out.println("Sala de Emergencias Atendiendo");
+				
+				System.out.println("HORA : " + tiempoTranscurrido);
+				
+				this.tiempoTranscurrido++;
+				
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+			
+		}
 	}
 	
 	public void pararSimulacion() {
@@ -40,10 +70,20 @@ public class Simulador implements Runnable {
 		
 	}
 	
-	@Override
-	public void run() {
+	public static void main(String[] args) {
+		Cubiculo c = new Cubiculo(3, TipoPaciente.QUEMADURAS);
+		Cubiculo c2 = new Cubiculo(3, TipoPaciente.QUEMADURAS);
 		
+		Paciente p = new Paciente("Beimar");
+		Paciente p2 = new Paciente("juan");
+
+		
+		SalaEmergencias sala = new SalaEmergencias();
+		sala.agregarCubiculo(c);
+		sala.agregarCubiculo(c2);
+		
+		Simulador s = new Simulador();
+		s.iniciarSimulacion();
 	}
-	
 	
 }
