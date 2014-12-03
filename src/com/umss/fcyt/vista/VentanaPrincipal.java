ea34d0b8ef47e16f773090a53722a18c81ccdc15
beimarhuarachi <beimar020@gmail.com>
2014-12-02 21:20:08 -0400

    add the library for the view and an icon

---
+++ src/com/umss/fcyt/vista/VentanaPrincipal.java
@@ -0,0 +1,50 @@
+package com.umss.fcyt.vista;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.border.EmptyBorder;
+
+import java.awt.Toolkit;
+
+public class VentanaPrincipal extends JFrame {
+
+	private JPanel contentPane;
+
+	
+	public static void main(String[] args) {
+		try {
+			UIManager.setLookAndFeel("com.jtattoo.plaf.acryl.AcrylLookAndFeel");
+			//UIManager.setLookAndFeel("com.seaglasslookandfeel.SeaGlassLookAndFeel");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedLookAndFeelException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		VentanaPrincipal frame = new VentanaPrincipal();
+		frame.setVisible(true);
+	}
+
+	public VentanaPrincipal() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage("imagenes/icono1.png"));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
